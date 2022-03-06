aae8e8c72396c055fa95d0bad78c1f2b06cb9265
jesse <sandberg.jesse@gmail.com>
2022-03-06 10:12:29 -0500

    changed name to make sense

--- polarH3.tcl
+++ polarH3.tcl
@@ -367,7 +367,7 @@ proc cell_prep {outfile} {
 ;########################################################################################
 ;# polarHeight Function
 
-proc polarHeightByShell {outfile} {
+proc polarHeightByFrame {outfile} {
 
 
     set Rmin 0
@@ -637,7 +637,7 @@ proc run_mult {list_of_systems} {
         puts $gro
         puts $xtc
         animate delete beg 0 end 0 skip 0 top
-        polarHeightByShell $item
+        polarHeightByFrame $item
         mol delete top
     }
 }
