c83899b8d830e23a6eb5df4892022c45faba687a
Jesse Sandberg <js2746@belenus.ccib.rutgers.edu>
2022-03-08 16:03:23 -0500

    added ability to compare systems

--- plotting/AvgOverTheta_vec.py
+++ plotting/AvgOverTheta_vec.py
@@ -4,15 +4,18 @@ import warnings
 
 
 
-#name_list = ["PO"]
-#name_list = ["DL", "DT", "DG", "DX", "PO", "DB", "DY", "DO", "DP"]
-#name_list = ["DL", "DT", "DX", "DB", "DP"]
-name_list = ["DG", "DY", "DO"]
-#name_list = ["DP", "DO", "PO"]
-#name_list = {"DL","DY"}
-#name_list = {"DB","../../scripts/PolarHeightBinning/results/DB"}
-#field_list = ["zone","ztwo","zplus","zzero"]
-field_list = ['zone', 'ztwo']
+allsys = ["DL", "DT", "DG", "DX", "PO", "DB", "DY", "DO", "DP"]
+satsys = ["DL", "DT", "DX", "DB", "DP"]
+monounsatsys = ["DG", "DY", "DO"]
+fivebeads = ["DB","DG"]
+fourbeads = ["DP", "DO", "PO"]
+threebeads = {"DL","DY"}
+
+sys_list = [allsys, satsys, monounsatsys, fivebeads, fourbeads, threebeads]
+sys_name_list = ["allsys", "satsys", "monounsatsys", "fivebeads", "fourbeads", "threebeads"]
+
+#field_list = ['zone', 'ztwo']
+field_list = ['zone', 'ztwo', 'zzero', 'zplus']
 
 colordict = {
 	"DT": "green",
@@ -27,14 +30,21 @@ colordict = {
 	"../../scripts/PolarHeightBinning/results/DB": "deepskyblue"
 }
 
-fig = plt.figure(figsize = (5,5))
-plt.xlim(0,70)
-plt.ylim(-50,20)
-plt.gca().set_aspect('equal',adjustable='box')
+fielddict = {
+	"zone" : "solid",
+	"ztwo" : "solid",
+	"zzero" : "dotted",
+	"zplus" : "dashed"
+}
 
-x = np.arange(3,70,6)
 
-for name in name_list:
+for name in allsys:
+	fig = plt.figure(figsize = (5,5))
+	plt.xlim(0,70)
+	plt.ylim(-50,20)
+	plt.gca().set_aspect('equal',adjustable='box')
+
+	x = np.arange(3,70,6)
 	for field in field_list:
 		data = np.load(name+"."+field+".avgheight.npy")
 		#for row in range(data.shape[0]):
@@ -44,12 +54,49 @@ for name in name_list:
 		#		data[row,:] = np.nan
 		with warnings.catch_warnings():
 			warnings.simplefilter("ignore", category=RuntimeWarning)
-			z_vals_lower=np.nanmean(data, axis=1)
+			z_vals=np.nanmean(data, axis=1)
 		
-		plt.plot(x,z_vals_lower,color=colordict[name])
+		plt.plot(x,z_vals,color=colordict[name],linestyle=fielddict[field])
 		X = [28.116,28.116]
 		Y = [-1,5]
 		plt.plot(X,Y,'k:')
 
 
-plt.show()
+	fig.set_size_inches(6,6)
+	plt.savefig(name+"_avgovertheta.png", dpi = 700)
+	plt.clf()
+	plt.close()
+
+'''
+for system in sys_list:
+	fig = plt.figure(figsize = (5,5))
+	plt.xlim(0,70)
+	plt.ylim(-50,20)
+	plt.gca().set_aspect('equal',adjustable='box')
+
+	x = np.arange(3,70,6)
+
+	for name in system:
+		for field in field_list:
+			data = np.load(name+"."+field+".avgheight.npy")
+			#for row in range(data.shape[0]):
+			#	nonzerocount = np.count_nonzero(data[row,:])
+			#	nancount = np.count_nonzero(np.isnan(data[row,:]))
+			#	if (nancount/nonzerocount) >= 0.2:
+			#		data[row,:] = np.nan
+			with warnings.catch_warnings():
+				warnings.simplefilter("ignore", category=RuntimeWarning)
+				z_vals=np.nanmean(data, axis=1)
+			
+			plt.plot(x,z_vals,color=colordict[name])
+			X = [28.116,28.116]
+			Y = [-1,5]
+			plt.plot(X,Y,'k:')
+
+
+	fig.set_size_inches(6,6)
+	plt.savefig(sys_name_list[counter]+"_avgovertheta.png", dpi = 700)
+	plt.clf()
+	plt.close()
+	counter += 1
+'''
