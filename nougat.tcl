3ab4a547cca774ba6a705dd0b7b08e58710ec0d8
Jesse Sandberg <js2746@belenus.ccib.rutgers.edu>
2022-08-22 10:39:40 -0400

    bug fixing

--- nougat.tcl
+++ nougat.tcl
@@ -158,7 +158,7 @@ proc start_nougat {system d1 N2 start end step polar} {
 
     ;# run nougat twice, once to compute height and density and once to compute
     ;# lipid tail vectors and order parameters
-    #run_nougat $system $important_variables $bindims $polar "height_density" 
+    run_nougat $system $important_variables $bindims $polar "height_density" 
     run_nougat $system $important_variables $bindims $polar "tilt_order" 
 
 }
@@ -288,7 +288,7 @@ proc run_nougat {system important_variables bindims polar quantity_of_interest} 
                 set orders [order_params [dict keys $selections] $xvals_list $yvals_list $zvals_list]
                 set outfiles [do_tilt_order_binning $res_dict $outfiles $leaflet_list $lipid_list $tilts $orders $tail_list $selex]
             }
-            
+
             ;# Now that all information has been binned, print it to files
             dict for {key val} [dict get $outfiles $selex] {
                 print_frame $N1 $outfiles $key $d1 $min $N2 $polar $selex
