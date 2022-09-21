816f97786fc24af216460c86d8b6df25fec6c294
Jesse Sandberg <js2746@belenus.ccib.rutgers.edu>
2022-09-21 09:36:18 -0400

    worked on new leaflet check

--- utilities/helper_procs.tcl
+++ utilities/helper_procs.tcl
@@ -31,24 +31,34 @@ proc fit_all_tails { tail_length list_of_tail_coords lsqnormfactor} {
   return $vectors
 }
 
+# Concatenates list items into one long string, separated by 
+# spaces on either side of the $delimiter.
+# "NULL" wil result in a single space as the delimiter
+# "or" will also enclose list items in parentheses for use as 
+# atomselection text. 
 proc cat_list { inputlist delimiter} {
-    if {$delimiter eq "NULL"} {
-        set output [lindex $inputlist 0]
-    } else {
+    if {$delimiter eq "or"} {
         set output "([lindex $inputlist 0])"
+    } else {
+        set output [lindex $inputlist 0]
     }
     
     for {set i 1} {$i < [llength $inputlist]} {incr i} {
-        if {$delimiter eq "NULL"} {
-            set output "${output} [lindex $inputlist $i]"
-        } else {
+        if {$delimiter eq "or"} {
             set element "([lindex $inputlist $i])"
             set output "${output} or ${element}"
+        } elseif {$delimiter eq "NULL"} {
+            set output "${output} [lindex $inputlist $i]"
+        } else {
+            set output "${output} $delimiter [lindex $inputlist $i]"
         }
     }
     return $output
 }
 
+# Returns a list of lists containing the starting beads and ending beads for a 
+# given lipid's acyl chains.
+# E.G. POPC start beads would be "C1A C1B" and end beads would be "C4A C4B"
 proc heads_and_tails {species taillist} {
     for {set i 0} {$i < [llength $taillist]} {incr i} {
         set startbead []
@@ -57,11 +67,45 @@ proc heads_and_tails {species taillist} {
             lappend startbead [lindex $tail 0]
             lappend endbead [lindex $tail end]
         }
-        lappend startsellist "resname [lindex $species $i] and name [cat_list $startbead "NULL"]"
-        lappend endsellist "resname [lindex $species $i] and name [cat_list $endbead "NULL"]"
+        lappend startsellist [cat_list $startbead "NULL"]
+        lappend endsellist [cat_list $endbead "NULL"]
     }
-    set startbeadseltext [cat_sel $startsellist "or"]
-    set endbeadseltext [cat_sel $endsellist "or"]
+
+    return [list $startsellist $endsellist]
+}
+
+proc new_leaflet_check {frm species heads_and_tails window} {
+    set starts [lindex $heads_and_tails 0]
+    set ends [lindex $heads_and_tails 1]
+
+    for {set i 0} {$i < [llength $species]} {incr i} {
+        set lipidtype [lindex $species $i]
+        set total_sel [atomselect top "resname $lipidtype" frame $frm]
+        set species_bead_num [llength [lsort -unique [$total_sel get name]]]
+        set startnames [lindex $starts $i]
+        set endnames [lindex $ends $i]
+        set numbeads [llength $startnames]
+        set start_sel [atomselect top "resname $lipidtype and name $startnames" frame $frm]
+        set end_sel [atomselect top "resname $lipidtype and name $endnames" frame $frm]
+        set start_z [$start_sel get z]
+        set end_z [$end_sel get z]
+        $start_sel delete
+        $end_sel delete
+        set diff [vecexpr $start_z $end_z sub]
+        set userlist []
+        for {set j 0} {$j < [llength $start_z]} {set j [expr $j+$numbeads]} {
+            set avgheight [vecexpr [lrange $diff $j [expr $j+$numbeads-1]] mean]
+            if {$avgheight > $window} {
+                lappend userlist [lrepeat $species_bead_num 1.0]
+            } elseif {$avgheight < -$window} {
+                lappend userlist [lrepeat $species_bead_num 2.0]
+            } else {
+                lappend userlist [lrepeat $species_bead_num 3.0]
+            }
+        }
+        $total_sel set user [cat_list $userlist "NULL"]
+        $total_sel delete
+    } 
 }
 
 # leaflet_check checks whether a lipid has flipped from one leaflet to another.
@@ -413,6 +457,45 @@ proc tail_analyzer { species } {
     }
 
     return $taillist
+}
+
+proc lipid_analyzer {species acyl_names} {
+    set numlist []
+    foreach lipid $species {
+        set sel [atomselect top "resname $lipid"]
+        set numbeads [llength [lsort -unique [$sel get name]]]
+        $sel delete
+        lappend numlist $numbeads
+    }
+
+    set starts_and_ends [heads_and_tails $species $acyl_names]
+    set starts [lindex $starts_and_ends 0]
+    set ends [lindex $starts_and_ends 1]
+    
+    ;# figure out how many tails are in each lipid
+    set lenlist []
+    for {set i 0} {$i < [llength $species]} {incr i} {
+        lappend lenlist [llength [lindex $starts $i]]
+    }
+    set uniquelengths [lsort -unique $lenlist]
+
+    foreach length $uniquelengths {
+        set startsel []
+        set endsel []
+        set indices [lsearch -all $lenlist $length]
+        foreach index $indices {
+            set startseltext "resname [lindex $species $index] and name [lindex $starts $i]"
+            set endseltext "resname [lindex $species $index] and name [lindex $ends $i]"
+            lappend startsel $startseltext
+            lappend endsel $endseltext
+        }
+        set uniquelengthstartseltext [cat_list $startsel "or"]
+        set uniquelengthendseltext [cat_list $endsel "or"]
+        dict set lipids $length start $uniquelengthstartseltext
+        dict set lipids $length end $uniquelengthendseltext
+    }
+
+    return $lipids
 }
 
 ;# tail_numberer changes user3 to hold a tail number.
