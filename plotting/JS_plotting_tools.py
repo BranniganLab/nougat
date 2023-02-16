7b2a3627e5524b9da0b8303eb5c55caf1af506d4
jesse <sandberg.jesse@gmail.com>
2023-02-16 11:43:56 -0500

    modified for transfer

--- plotting/JS_plotting_tools.py
+++ plotting/JS_plotting_tools.py
@@ -181,9 +181,10 @@ def avg_eps_t0_over_theta(systems):
 			warnings.simplefilter("ignore", category=RuntimeWarning)
 			z_vals=np.nanmean(eps_data, axis=1)
 		maxval = len(z_vals)
+		print(maxval)
 		x = np.arange(5,(maxval*10+5),10) / 10
 		axs.plot(x,z_vals,color=colordict[system])
-	plt.savefig("unsatsys_avg_epsilonovertheta_combo.pdf", dpi = 700)
+	plt.savefig("satsys_avg_epsilonovertheta_combo.pdf", dpi = 700)
 	plt.clf()
 	plt.close()
 
@@ -343,9 +344,9 @@ def plot_average_area_per_lipid(systems):
 
 if __name__ == "__main__": 
 	#diff_mid_interface(["lgDT", "lgDY", "lgDG", "lgDO", "lgDP", "lgDL", "lgDX", "lgDB"], "5x29", "polar")
-	#avg_eps_t0_over_theta(["DT", "DL", "DP", "DB", "DX"])
+	avg_eps_t0_over_theta(["DT", "DL", "DP", "DB", "DX"])
 	#measure_H_epsilon_corr(["lgPO"], "empty")
-	avg_eps_t0_over_theta(["DY", "DO", "DG"])
+	#avg_eps_t0_over_theta(["DY", "DO", "DG"])
 	#measure_t0(["lgPO", "lgDG", "lgDY", "lgDT0", "lgDO", "lgDP", "lgDL", "lgDX", "lgDB"], "5x29")
 	#diff_mid_interface(["lgPO"], "7k3g")
 	#measure_H(["PO", "DG", "DY", "DT", "DL", "DO", "DP", "DX", "DB"], "5x29")
