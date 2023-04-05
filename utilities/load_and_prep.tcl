1dbea0e928a84930a4cffd0d36775d57b12999a9
Jesse Sandberg <js2746@belenus.ccib.rutgers.edu>
2023-04-05 10:22:07 -0400

    changed path to trajpath

--- utilities/load_and_prep.tcl
+++ utilities/load_and_prep.tcl
@@ -1,6 +1,6 @@
 proc load_and_prep {sysname trajpath configpath groname xtcname first last step d1 d2 polar} {
-	set gro "${path}/${groname}"
-	set xtc "${path}/${xtcname}"
+	set gro "${trajpath}/${groname}"
+	set xtc "${trajpath}/${xtcname}"
 	mol new $gro
 	mol addfile $xtc first ${first} last ${last} step ${step} waitfor all
 	animate delete beg 0 end 0 skip 0 top
