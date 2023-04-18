fe5692420c4309f3f0b8f6a93f02a67c75c24363
Jesse Sandberg <js2746@belenus.ccib.rutgers.edu>
2023-04-18 10:06:26 -0400

    moved measure_t0 to utils

--- plotting/thickness.py
+++ plotting/thickness.py
@@ -7,24 +7,6 @@ import os
 from utils import *
 #from code_review2 import *
 
-def measure_t0(zone, ztwo, coordsys):
-  thickness = zone-ztwo
-
-  avgthickness = calc_avg_over_time(thickness)
-
-  if coordsys == "cart":
-    leftcol = np.mean(avgthickness[:,0])
-    rightcol =  np.mean(avgthickness[:,-1])
-    toprow =  np.mean(avgthickness[0,:])
-    botrow =   np.mean(avgthickness[-1,:])
-    avgt0 = (leftcol+rightcol+toprow+botrow)/4.0
-  elif coordsys == "polar":
-    avgt0 = np.mean(avgthickness[-1:])
-
-  avgt0 = avgt0/2.0
-
-  return avgt0
-
 def calculate_thickness(sys_name, bead, coordsys, inclusion, polar, dims, scale_dict):
   N1_bins, d1, N2_bins, d2, Nframes, dim1vals, dim2vals = dims 
   zone = np.load('npy/'+sys_name+'.zone.'+bead+'.'+coordsys+'.height.npy')
