cf2a921dd7021a073d6214a7bb7629e978f0dc2b
Jesse Sandberg <js2746@belenus.ccib.rutgers.edu>
2023-07-21 10:18:42 -0400

    finished with run_nougat_test.tcl

--- testing_materials/run_nougat_test.tcl
+++ testing_materials/run_nougat_test.tcl
@@ -1,11 +1,12 @@
 source ~/PolarHeightBinning/nougat.tcl
 
 proc load_and_run_test {trajpath groname xtcname test_name config d1 d2 polar} {
-	set gro "${trajpath}/${groname}"
-	set xtc "${trajpath}/${xtcname}"
-	mol new $gro
-	mol addfile $xtc waitfor all
-	animate delete beg 0 end 0 skip 0 top
+	cd $trajpath
+	mol new $groname
+	if {$xtcname != 0} {
+		mol addfile $xtcname waitfor all
+		animate delete beg 0 end 0 skip 0 top
+	}
 	start_nougat $test_name $config $d1 $d2 0 -1 1 $polar
 	mol delete top
 }
@@ -13,13 +14,20 @@ proc load_and_run_test {trajpath groname xtcname test_name config d1 d2 polar} {
 ;# Put your tests below
 
 ;# E protein cartesian
-load_and_run_test ~/PolarHeightBinning/testing_materials/E-protein_trajectory DT_test.gro DT_test.xtc E-protein ~/PolarHeightBinning/testing_materials/E-protein_trajectory/nougat_config_test_E-protein.txt 5 5 0
+set path "/home/js2746/PolarHeightBinning/testing_materials/E-protein_trajectory"
+load_and_run_test $path DT_test.gro DT_test.xtc E-protein /home/js2746/PolarHeightBinning/testing_materials/E-protein_trajectory/nougat_config_test_E-protein.txt 5 5 0
 
 ;# E protein polar
-load_and_run_test ~/PolarHeightBinning/testing_materials/E-protein_trajectory DT_test.gro DT_test.xtc E-protein ~/PolarHeightBinning/testing_materials/E-protein_trajectory/nougat_config_test_E-protein.txt 3 12 1
+set path "/home/js2746/PolarHeightBinning/testing_materials/E-protein_trajectory"
+load_and_run_test $path DT_test.gro DT_test.xtc E-protein /home/js2746/PolarHeightBinning/testing_materials/E-protein_trajectory/nougat_config_test_E-protein.txt 3 12 1
 
 ;# Molecularly flat membrane cartesian
-
-
+set path "/home/js2746/PolarHeightBinning/testing_materials/flat_surface_test"
+load_and_run_test $path flat.gro 0 flat /home/js2746/PolarHeightBinning/testing_materials/E-protein_trajectory/nougat_config_test_E-protein.txt 5 5 0
 
 ;# Molecularly flat membrane polar
+set path "/home/js2746/PolarHeightBinning/testing_materials/flat_surface_test"
+load_and_run_test $path flat.gro 0 flat /home/js2746/PolarHeightBinning/testing_materials/E-protein_trajectory/nougat_config_test_E-protein.txt 3 12 1
+
+;# exit
+exit
