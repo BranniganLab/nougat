08f079dfaef6dbf6d6c809e4e5530fe47e46f94f
Jesse Sandberg <js2746@belenus.ccib.rutgers.edu>
2023-07-21 10:53:50 -0400

    removed test line

--- testing_materials/run_nougat_py_test.sh
+++ testing_materials/run_nougat_py_test.sh
@@ -1,8 +1,8 @@
 #! /bin/sh/
 
 cd /home/js2746/PolarHeightBinning/testing_materials/E-protein_trajectory/test_cart_5_5_0_-1_1
-echo "I'm here"
 python3 /home/js2746/PolarHeightBinning/plotting/nougat.py test /home/js2746/PolarHeightBinning/plotting/nougat_plot_config.txt
 
 cd /home/js2746/PolarHeightBinning/testing_materials/E-protein_trajectory/test_polar_3_12_0_-1_1
 python3 /home/js2746/PolarHeightBinning/plotting/nougat.py test /home/js2746/PolarHeightBinning/plotting/nougat_plot_config.txt -p
+
