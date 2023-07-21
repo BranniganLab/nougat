2fe40bcc29b3c87742154a4ad4db50909beb6aa8
Jesse Sandberg <js2746@belenus.ccib.rutgers.edu>
2023-07-21 11:48:55 -0400

    added flat surface test nougat commands

--- testing_materials/run_nougat_py_test.sh
+++ testing_materials/run_nougat_py_test.sh
@@ -6,3 +6,8 @@ python3 /home/js2746/PolarHeightBinning/plotting/nougat.py test /home/js2746/Pol
 cd /home/js2746/PolarHeightBinning/testing_materials/E-protein_trajectory/test_polar_3_12_0_-1_1
 python3 /home/js2746/PolarHeightBinning/plotting/nougat.py test /home/js2746/PolarHeightBinning/plotting/nougat_plot_config.txt -p
 
+cd /home/js2746/PolarHeightBinning/testing_materials/flat_surface_test/test_cart_5_5_0_-1_1
+python3 /home/js2746/PolarHeightBinning/plotting/nougat.py test /home/js2746/PolarHeightBinning/plotting/nougat_plot_config.txt
+
+cd /home/js2746/PolarHeightBinning/testing_materials/flat_surface_test/test_polar_3_12_0_-1_1
+python3 /home/js2746/PolarHeightBinning/plotting/nougat.py test /home/js2746/PolarHeightBinning/plotting/nougat_plot_config.txt -p
