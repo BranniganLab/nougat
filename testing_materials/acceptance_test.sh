ca3db067f39b003df75abdbc1442c8f9cbe01f54
Jesse Sandberg <js2746@belenus.ccib.rutgers.edu>
2023-07-21 10:47:54 -0400

    made bash script for running nougat.py

--- testing_materials/acceptance_test.sh
+++ testing_materials/acceptance_test.sh
@@ -1,15 +1,16 @@
 #! /bin/sh
 
-# Run nougat.tcl and nougat.py on test system(s)
-vmd -e ~/PolarHeightBinning/testing_materials/run_nougat_test.tcl
-python3 ~/PolarHeightBinning/testing_materials/run_nougat_test.py
+# Run nougat.tcl and nougat.py on test systems
+vmd -e /home/js2746/PolarHeightBinning/testing_materials/run_nougat_test.tcl
+bash /home/js2746/PolarHeightBinning/testing_materials/run_nougat_py_test.sh
 
 # Compare new test results to saved test results
-result='python3 ~/PolarHeightBinning/testing_materials/compare_test_results.py'
+result=`python3 ~/PolarHeightBinning/testing_materials/compare_test_results.py`
 
 # Clean up if all tests passed
 if [ "$result" == "True" ]; then
-	rm -r ~/PolarHeightBinning/testing_materials/... 
+	rm -r ~/PolarHeightBinning/testing_materials/E-protein_trajectory/test_*
+	rm -r ~/PolarHeightBinning/testing_materials/flat_surface_test/test_*
 fi
 
 # Sound the all clear
