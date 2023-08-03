18e982a9bdddc4098e0af3d293bc15cd9018b88d
Jesse Sandberg <js2746@belenus.ccib.rutgers.edu>
2023-08-03 10:27:52 -0400

    ammended path to unit tests script

--- testing_materials/acceptance_test.sh
+++ testing_materials/acceptance_test.sh
@@ -2,7 +2,7 @@
 
 # Run unit tests
 # cd ../test/ 
-# vmd -dispdev none -e ./unit_test.test > unit_tests.log
+# vmd -dispdev none -e ./run.tcl > unit_tests.log
 # cd ../testing_materials/
 
 # Run nougat.tcl and nougat.py on test systems
