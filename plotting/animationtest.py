b718e69861d4f2d22f4ad5b8fa22483c3b6e013d
Jesse Sandberg <js2746@belenus.ccib.rutgers.edu>
2023-08-07 12:32:51 -0400

    bad save command

--- plotting/animationtest.py
+++ plotting/animationtest.py
@@ -9,28 +9,32 @@ Created on Fri Aug  4 11:30:26 2023
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import animation
+from matplotlib.animation import PillowWriter
 from utils import *
 import os
 
 
 def make_animated_heatmap(data, coordsys, dim1vals, dim2vals, Vmax, Vmin, colorbar):
     fig = plt.figure()
+    if coordsys == "polar":
+        ax = plt.subplot(projection="polar")
+    else:
+        ax = plt.subplot()
+    plt.axis('off')
+    label = ax.text(1.5 * np.pi, 200, 'Frame 0', ha='center', va='center')
 
     def init():
-        if coordsys == "polar":
-            ax = plt.subplot(projection="polar")
-        else:
-            ax = plt.subplot()
-        plt.axis('off')
-        c = plt.pcolormesh(dim2vals, dim1vals, data[:,:,0], cmap="RdBu_r", zorder=0)
+        c = plt.pcolormesh(dim2vals, dim1vals, data[:, :, 0], cmap="RdBu_r", vmax=Vmax, vmin=Vmin, zorder=0)
         plt.colorbar(c)
         return ax
 
     def animate(frame):
-        c = plt.pcolormesh(dim2vals, dim1vals, data[:, :, frame], cmap="RdBu_r", zorder=0)
+        c = plt.pcolormesh(dim2vals, dim1vals, data[:, :, frame], cmap="RdBu_r", vmax=Vmax, vmin=Vmin, zorder=0)
+        label.set_text('Frame ' + str(frame))
         return c
 
     anim = animation.FuncAnimation(fig, animate, init_func=init, frames=np.shape(data)[2], repeat=False)
+    anim.save("test.gif", progress_callback=lambda i, n: print(f'Saving frame {i} of {n}'), dpi=500, writer=PillowWriter(fps=3))
     plt.show()
 
 
