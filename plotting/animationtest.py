ac85b91b456a4a553f9198dc7e93e7c9d07f5f71
Jesse Sandberg <js2746@belenus.ccib.rutgers.edu>
2023-08-07 16:34:19 -0400

    working example

--- plotting/animationtest.py
+++ plotting/animationtest.py
@@ -22,20 +22,17 @@ def make_animated_heatmap(data, coordsys, dim1vals, dim2vals, Vmax, Vmin, colorb
         ax = plt.subplot()
     plt.axis('off')
     label = ax.text(1.5 * np.pi, 200, 'Frame 0', ha='center', va='center')
-
-    def init():
-        c = plt.pcolormesh(dim2vals, dim1vals, data[:, :, 0], cmap="RdBu_r", vmax=Vmax, vmin=Vmin, zorder=0)
-        plt.colorbar(c)
-        return ax
+    c = plt.pcolormesh(dim2vals, dim1vals, data[:, :, 0], cmap="RdBu_r", vmax=Vmax, vmin=Vmin, zorder=0)
+    plt.colorbar(c)
 
     def animate(frame):
-        c = plt.pcolormesh(dim2vals, dim1vals, data[:, :, frame], cmap="RdBu_r", vmax=Vmax, vmin=Vmin, zorder=0)
+        c.set_array(data[:, :, frame])
         label.set_text('Frame ' + str(frame))
         return c
 
-    anim = animation.FuncAnimation(fig, animate, init_func=init, frames=np.shape(data)[2], repeat=False)
-    anim.save("test.gif", progress_callback=lambda i, n: print(f'Saving frame {i} of {n}'), dpi=500, writer=PillowWriter(fps=3))
-    plt.show()
+    anim = animation.FuncAnimation(fig, animate, frames=10, interval=333, repeat=False)
+    anim.save("/home/js2746/PolarHeightBinning/plotting/test.gif", progress_callback=lambda i, n: print(f'Saving frame {i} of {n}'))
+    plt.close()
 
 
 os.chdir("/home/js2746/Bending/PC/whole_mols/5x29/40nmSystems/dm1/lgPO/lgPO_polar_5_10_0_-1_1")
