efa5fe03c85230c836b1a766e04cdbfa73073bce
Jesse Sandberg <js2746@belenus.ccib.rutgers.edu>
2023-08-18 12:00:49 -0400

    fixed read_log function to work for multiple lipid types

--- plotting/utils.py
+++ plotting/utils.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 from matplotlib import animation
 import numpy as np
 import warnings
+import os
 
 
 def strip_blank_lines(file):
@@ -230,30 +231,59 @@ def mostly_empty(data_array):
 
 
 def read_log(sys_name, coordsys):
-    # this proc is not robust to multiple lipid species in the same system!!
-    # specifically, species with differing headnames
-    names_dict = {}
-    names_dict['beads_list'] = []
+    """
+    Read log file output by nougat.tcl and save important info for later.
+
+    Parameters
+    ----------
+    sys_name : STRING
+        The name of the system you designated with nougat.tcl and nougat.py.
+    coordsys : STRING
+        'cart' or 'polar'.
+
+    Returns
+    -------
+    system_dict : DICT
+        A dictionary containing the list of lipid species, their respective \
+            headnames, their respective density normalization factors, and \
+            the bin sizes.
+
+    """
+    system_dict = {}
+
     # open log file
     with open("tcl_output/" + sys_name + "." + coordsys + ".log", "r+") as log_file:
         lines = [line.rstrip('\n') for line in log_file]
 
-        # get contents of line 1 and save as species_list
-        names_dict['species_list'] = lines[1].split(' ')
+        species_list = []
+        # get all lipid species names from line 2
+        for species in lines[1].split(' '):
+            species_list.append(species)
+        system_dict["species"] = species_list
 
-        # get contents of line 2 and save as beads_list
-        headnames = lines[2].split(' ')
-        filename = headnames[0]
-        for indx in range(1, len(headnames)):
-            filename = filename + "." + headnames[indx]
-        if filename not in names_dict['beads_list']:
-            names_dict['beads_list'].append(filename)
+        # get headnames from headnames section
+        headnames_start_line = lines.index("#HEADNAMES") + 1
+        system_dict['headnames'] = {}
+        for line in range(len(system_dict["species"])):
+            names_line = lines[headnames_start_line].split(':')
+            system_dict["headnames"][names_line[0]] = names_line[1]
+            headnames_start_line += 1
 
-        # get density norm factor
-        start_line = lines.index("#DENSITY NORMALIZATION") + 1
-        names_dict['density_norm'] = float(lines[start_line].split(":")[1])
+        # get density norm info from density section
+        density_start_line = lines.index("#DENSITY NORMALIZATION") + 1
+        system_dict["density_norm"] = {}
+        for line in range(len(system_dict["species"])):
+            names_line = lines[density_start_line].split(':')
+            system_dict["density_norm"][names_line[0]] = names_line[1]
+            density_start_line += 1
 
-    return names_dict
+        # get bin size info from bin info section
+        bin_start_line = lines.index("#BIN INFO") + 1
+        N1, N2 = lines[bin_start_line].split(' ')
+        d1, d2 = lines[bin_start_line + 1].split(' ')
+        system_dict['bin_info'] = {"N1": N1, "N2": N2, "d1": d1, "d2": d2}
+
+    return system_dict
 
 
 def plot_maker(dim1vals, dim2vals, data, name, field, Vmax, Vmin, protein, dataname, bead, coordsys, config_dict):
@@ -701,3 +731,8 @@ def measure_quant_in_empty_sys(path, system, coordsys, quantity):
         warnings.simplefilter("ignore", category=RuntimeWarning)
         avg = np.nanmean(data)
     return avg
+
+
+if __name__ == "__main__":
+    os.chdir('../testing_materials/E-protein_trajectory/E-protein_cart_5_5_0_-1_1/')
+    read_log('E-protein', 'cart')
