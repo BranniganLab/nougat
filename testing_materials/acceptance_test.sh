61946252c983299e7d9eb7bf87e3d4f791499938
Jesse Sandberg <js2746@belenus.ccib.rutgers.edu>
2023-09-26 09:41:48 -0400

    turned off log

--- testing_materials/acceptance_test.sh
+++ testing_materials/acceptance_test.sh
@@ -1,13 +1,13 @@
 #! /bin/sh
 
 # Run unit tests
-# cd ../test/ 
-# vmd -dispdev none -e ./run_unit_tests.tcl > unit_tests.log
-# cd ../testing_materials/
+cd ../test/ 
+vmd -dispdev none -e ./run_unit_tests.tcl 2>&1 | tee -a pytest.log
+cd ../testing_materials/
 
 # Run nougat.tcl and nougat.py on test systems
 vmd -dispdev none -e ./run_nougat_test.tcl
-bash ./run_nougat_py_test.sh
+bash ./run_nougat_py_test.sh 2>&1 | tee -a nougpy.log
 
 # Run test battery on resulting files; print to file and terminal (tee)
 python3 -m pytest tests/ 2>&1 | tee -a pytest.log
