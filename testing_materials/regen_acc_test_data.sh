26c1e5615bafa27061a1258f166e9e84c0823b7c
Jesse Sandberg <js2746@belenus.ccib.rutgers.edu>
2023-11-01 15:46:43 -0400

    added git commands to regen data protocol

--- testing_materials/regen_acc_test_data.sh
+++ testing_materials/regen_acc_test_data.sh
@@ -1,5 +1,10 @@
 #! /bin/sh/
 
+git rm -r ./E-protein_trajectory/E-protein_*
+git rm -r ./flat_surface_test/flat_*
+
+git commit -m 'deleting old acceptance testing ref values'
+
 vmd -dispdev none -e ./regen_acc_test_data.tcl
 
 # Add any systems you want nougat to run below
@@ -19,3 +24,8 @@ cd ../..
 cd ./flat_surface_test/flat_polar_3_12_0_-1_1
 python3 ../../../plotting/nougat.py ../../../plotting/nougat_plot_config.txt -p
 cd ../..
+
+git add ./E-protein_trajectory/E-protein_*
+git add ./flat_surface_test/flat_*
+
+git commit -m 'generating new acceptance testing ref values'
