5fe7909524dc7e642f6ccf066870a366c339a87f
Jesse Sandberg <js2746@belenus.ccib.rutgers.edu>
2023-11-20 15:56:09 -0500

    removed nougat.py arg for config

--- testing_materials/regen_acc_test_data.sh
+++ testing_materials/regen_acc_test_data.sh
@@ -10,19 +10,19 @@ vmd -dispdev none -e ./regen_acc_test_data.tcl
 # Add any systems you want nougat to run below
 
 cd ./E-protein_trajectory/E-protein_cart_5_5_0_-1_1
-python3 ../../../plotting/nougat.py ../../../plotting/nougat_plot_config.txt
+python3 ../../../plotting/nougat.py
 cd ../..
 
 cd ./E-protein_trajectory/E-protein_polar_3_12_0_-1_1
-python3 ../../../plotting/nougat.py ../../../plotting/nougat_plot_config.txt -p
+python3 ../../../plotting/nougat.py -p
 cd ../..
 
 cd ./flat_surface_test/flat_cart_5_5_0_-1_1
-python3 ../../../plotting/nougat.py ../../../plotting/nougat_plot_config.txt
+python3 ../../../plotting/nougat.py
 cd ../..
 
 cd ./flat_surface_test/flat_polar_3_12_0_-1_1
-python3 ../../../plotting/nougat.py ../../../plotting/nougat_plot_config.txt -p
+python3 ../../../plotting/nougat.py -p
 cd ../..
 
 git add ./E-protein_trajectory/E-protein_*
