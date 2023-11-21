36a98beca27f032b0fb533563ecdfae846118c35
Jesse Sandberg <js2746@belenus.ccib.rutgers.edu>
2023-11-21 10:25:02 -0500

    added comments

--- testing_materials/acceptance_test.sh
+++ testing_materials/acceptance_test.sh
@@ -1,20 +1,23 @@
 #! /bin/sh
 
-# Remove old log files to prevent appending
+# Remove old log files to prevent accidental appending. || True is to suppress
+# error if no such file exists.
 rm nougpy.log || True
 rm pytest.log || True
 rm tcl_unit_test.log || True
 
-# Run unit tests
+# Run tcl unit tests and divert output to file AND to terminal (tee)
 cd ../test/ 
 vmd -dispdev none -e ./run_unit_tests.tcl 2>&1 | tee -a ../testing_materials/tcl_unit_test.log
 cd ../testing_materials/
+
+# Run python unit tests and divert output to file and terminal.
 
 # Run nougat.tcl and nougat.py on test systems
 vmd -dispdev none -e ./run_nougat_test.tcl
 bash ./run_nougat_py_test.sh 2>&1 | tee -a nougpy.log
 
-# Run test battery on resulting files; print to file and terminal (tee)
+# Run acceptance tests on resulting files
 python3 -m pytest tests/ 2>&1 | tee -a pytest.log
 
 # Sound the all clear
