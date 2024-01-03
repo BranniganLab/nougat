2328d182505c69ae3420ede1a1198a1eb56c616c
Jesse Sandberg <js2746@belenus.ccib.rutgers.edu>
2024-01-03 13:09:37 -0500

    minor edits

--- utilities/helper_procs.tcl
+++ utilities/helper_procs.tcl
@@ -359,7 +359,6 @@ proc printValue {file value endLine} {
 #       - min needs to be implemented 
 
 proc printFrame {N1 outfiles key d1 min N2 polar selex} {
-
     set file [dict get $outfiles $selex $key fname]
 
     ;# starts new line in outfile with bin values
