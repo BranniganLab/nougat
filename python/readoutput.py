cf53de0209ef0780ce4b90341f0f86f755dcab7c
JahmalEnnis <Jahmal.ennis@gmail.com>
2025-09-19 13:02:28 -0400

    upated files based on pull request

--- python/readoutput.py
+++ python/readoutput.py
@@ -8,7 +8,6 @@ Created on Tue Feb 25 16:42:51 2025
 
 import numpy as np
 import argparse
-import os
 from pathlib import Path
 
 def read_nougat_data(filepath,filename, seclen):
@@ -27,11 +26,15 @@ def read_nougat_data(filepath,filename, seclen):
     Returns
     -------
     systemArray : arr
-        Nougat text document converted to a numpy array.
+        Nougat text document converted to a Ndimensional numpy array.
 
     """
-    fullpath = Path(filepath+"/"+filename)
-    loadFile = np.genfromtxt(fullpath,dtype='str')
+    if isinstance(filepath, str):
+        filepath=Path(filepath)
+    fullpath = filepath.joinpath(filename)
+    loadFile = np.genfromtxt(fullpath, dtype='str')
+    array_shape = loadFile.shape[0]/seclen
+    assert isinstance(array_shape, int), "Section length gave non-integer array shape"
     systemArray = loadFile.reshape(int(loadFile.shape[0]/seclen), seclen, int(loadFile.shape[1]))
     return systemArray
 
@@ -53,17 +56,20 @@ def save_nougat_data(filepath, nparry, boole):
     None.
 
     """
+    if isinstance(filepath, str):
+        filepath=Path(filepath)
     if boole:
-        np.save(Path(filepath+"/"+"NGArrayData.npy"), nparry)
+        np.save(filepath.joinpath("NGArrayData.npy"), nparry)
     
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument("-p", "--path", type=str, default=os.getcwd(), help="Path to nougat output file directory")
+    parser.add_argument("-ip", "--inputpath", type=str, default=Path.cwd(), help="Path to nougat input file directory")
+    parser.add_argument("-op", "--outputpath", type=str, default=Path.cwd(), help="Path to nougat output file directory")
     parser.add_argument("-n", "--name", type=str, default="full_file.dat", help="Name of file")
     parser.add_argument("-l", "--seclen", type=int, default=6, help="Number of output varibles in each frame")
     parser.add_argument("-s", "--save", type=bool, default=False, help="Save Frame")
     args = parser.parse_args()
-    ngArray = read_nougat_data(args.path, args.name, args.seclen)
-    save_nougat_data(args.path, ngArray, args.save)
+    ngArray = read_nougat_data(args.inputpath, args.name, args.seclen)
+    save_nougat_data(args.outputpath, ngArray, args.save)
     
