d67211f6b6799ba4c75eb2f6dd0437ee8d527e16
Jesse Sandberg <js2746@viscosus.ccib.rutgers.edu>
2025-12-17 15:37:40 -0500

    renamed file

--- python/nougat/__init__.py
+++ python/nougat/__init__.py
@@ -7,13 +7,13 @@ Toolkit for analysis of membrane disruption by proteins and other inclusions
 __version__ = "1.0"
 __author__ = 'Brannigan Lab'
 __credits__ = 'Rutgers University - Camden'
-__all__=['nougat',
+__all__=['classes',
         'curvature',
         'gifmaker',
         'utils']
 
 
-from .nougat import *
+from .classes import *
 from .utils import *
 from .curvature import *
 from .gifmaker import *
