b82bdcd26c7b85100b658011b7f74c0a0f44dad2
Jesse Sandberg <js2746@viscosus.ccib.rutgers.edu>
2026-01-05 13:24:28 -0500

    refactored

--- python/nougat/make_pdb_of_field.py
+++ python/nougat/make_pdb_of_field.py
@@ -5,8 +5,40 @@ Created on Mon Jan  5 09:28:42 2026.
 
 @author: js2746
 """
-import numpy as np
 from nougat.utils import compute_bin_centers
+
+
+def make_pdb(filename, list_of_surfaces, list_of_names, bin_info, box_z=' 200.000'):
+    """
+    Make a pdb file from average height surfaces.
+
+    Parameters
+    ----------
+    filename : str or path
+        The name of the pdb file you wish to create.
+    list_of_surfaces : list of numpy ndarrays
+        List of 2D arrays containing the average height surface(s) you wish to
+        represent in your pdb.
+    list_of_names : list of str
+        The names of each surface. Limit 4 characters per name.
+    bin_info : named tuple
+        The lattice information (bin widths, number, coordinate system, etc).
+    box_z : str, OPTIONAL
+        The height in z of the box. Default is 200 A.
+
+    Returns
+    -------
+    None.
+
+    """
+    index = 0
+    box_x = format_coordinate(bin_info.N1 * bin_info.d1)
+    box_y = format_coordinate(bin_info.N2 * bin_info.d2)
+    with open(filename, "w") as pdb:
+        print(f'CRYST1 {box_x} {box_y} {box_z}  90.00  90.00  90.00 P 1           1', file=pdb)
+        for surface, name in zip(list_of_surfaces, list_of_names):
+            index = print_surface_to_pdb(surface, bin_info, pdb, index, name)
+        print("END", file=pdb)
 
 
 def print_surface_to_pdb(data, bin_info, f, index_num, field_name):
@@ -33,23 +65,12 @@ def print_surface_to_pdb(data, bin_info, f, index_num, field_name):
 
     """
     resid_num = 1
-
+    x_centers, y_centers = compute_bin_centers(bin_info)
     for d1bin in range(bin_info.N1):
         for d2bin in range(bin_info.N2):
             if str(data[d1bin][d2bin]) != "nan":
-                # calculate bin center in x and y
-                if bin_info.coordsys == "polar":
-                    r = bin_info.d1 * d1bin + 0.5 * bin_info.d1
-                    theta = d2bin * bin_info.d2 + 0.5 * bin_info.d2
-                    # x = rcos(theta); y = rsin(theta)
-                    x = r * np.cos(theta)
-                    y = r * np.sin(theta)
-                else:
-                    x = bin_info.d1 * d1bin + .5 * bin_info.d1
-                    y = bin_info.d2 * d2bin + .5 * bin_info.d2
-                    # cartesian systems are centered around the origin
-                    x = x - bin_info.d1 * bin_info.N1 / 2
-                    y = y - bin_info.d2 * bin_info.N2 / 2
+                x = x_centers[d1bin][d2bin]
+                y = y_centers[d1bin][d2bin]
 
                 # print row of .pdb file
                 print(
