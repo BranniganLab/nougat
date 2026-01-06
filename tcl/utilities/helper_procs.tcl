36d85a1e6ef079835714e365655594cbe3698fed
Jesse Sandberg <js2746@viscosus.ccib.rutgers.edu>
2026-01-06 13:22:59 -0500

    putting the minus sign back

--- tcl/utilities/helper_procs.tcl
+++ tcl/utilities/helper_procs.tcl
@@ -263,7 +263,7 @@ proc assignLeaflet {frm species tailTopsAndBottoms threshold} {
             ;# assign user value accordingly
             if {$heightDiff > $threshold} {
                 lappend userList [lrepeat $speciesBeadNum 1.0]
-            } elseif {$heightDiff < - $threshold} {
+            } elseif {$heightDiff < -$threshold} {
                 lappend userList [lrepeat $speciesBeadNum 2.0]
             } else {
                 lappend userList [lrepeat $speciesBeadNum 3.0]
