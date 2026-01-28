86abf3f08d86cf25adc4dc019d15d6c2e4744510
Jesse Sandberg <js2746@viscosus.ccib.rutgers.edu>
2026-01-28 09:24:45 -0500

    added error check

--- tcl/nougat.tcl
+++ tcl/nougat.tcl
@@ -192,6 +192,11 @@ proc run_nougat {system config_dict bindims polar quantity_of_interest foldernam
             $sel frame $frm 
             $sel update
 
+            ;# error check
+            if {[$sel num] == 0} {
+                error "There are no selected atoms in ${selex} on frame ${frm}"
+            }
+
             ;# assemble all data (x,y,z,user, etc) into a dict of lists
             set sel_info [getSelInfo $sel $ref_height]
 
